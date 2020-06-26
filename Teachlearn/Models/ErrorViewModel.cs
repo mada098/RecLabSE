12e1540e0015a72d56f56d7fbeaeb0c3a72370ac
mada098 <duumstering@gmail.com>
2020-06-26 17:01:49 +0300

    0.1
    + creare proiect Teachlearn

---
+++ Teachlearn/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teachlearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
