e9697fb67651736d1f04c50acf8f446927dfc931
mada098 <duumstering@gmail.com>
2020-07-09 22:49:32 +0300

    Revert "Merge branch 'master' of https://github.com/mada098/RecLabSE"
    This reverts commit 122a033b6e1b57e5e7a03878a9d86afd0daae720, reversing
    changes made to 68f67c1d3a86fa74de435ed1ec8519b97c13c390.

--- Teachlearn/Models/Assignment.cs
+++ Teachlearn/Models/Assignment.cs
@@ -8,7 +8,6 @@ namespace Teachlearn.Models
     public class Assignment
     {
         public Guid AssignmentID { get; set; }
-<<<<<<< HEAD
 
         public string AssignmentName { get; set; }
 
@@ -17,13 +16,5 @@ namespace Teachlearn.Models
         public int Grade { get; set; }
 
         public Classroom Classroom { get; set; }
-=======
-        public string AssignmentTitle { get; set; }
-        public string AssignmentContent { get; set; }
-        public DateTime DueDate { get; set; }
-        public Grade Grade { get; set; }
-        public Class Class { get; set; }
-
->>>>>>> 721b822073b521b87c9a8cde59179794b36c2fa5
     }
 }
--- Teachlearn/Models/Course.cs
+++ Teachlearn/Models/Course.cs
@@ -8,7 +8,6 @@ namespace Teachlearn.Models
     public class Course
     {
         public Guid CourseID { get; set; }
-<<<<<<< HEAD
 
         public string CourseName { get; set; }
 
@@ -17,10 +16,5 @@ namespace Teachlearn.Models
         public string DownloadLink { get; set; }
 
         public Classroom Classroom { get; set; }
-=======
-        public string CourseTitle { get; set; }
-        public string CourseContent { get; set; }
-        public Class Class { get; set; }
->>>>>>> 721b822073b521b87c9a8cde59179794b36c2fa5
     }
 }
